00af85977f899a4d7d89e85351700ca71203d2a7
supercollli <819169384@qq.com>
2020-12-21 16:26:56 +0800

    修改场景

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AITank
+{
+    public class Unit : MonoBehaviour
+    {
+        public float fullHealth = 100;
+        private float _curHealth;
+
+        public float health
+        {
+            get { return _curHealth / fullHealth; }
+            set { _curHealth = fullHealth * value; }
+        }
+
+        public virtual void Setup()
+        {
+            health = 1;
+            gameObject.SetActive(true);
+        }
+
+        public bool ApplyDamage(float point)
+        {
+            _curHealth -= point;
+            if (!(_curHealth <= 0)) return false;
+            Dead();
+            return true;
+        }
+
+        public virtual void Dead()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    public float physicSteps = 0.01f;
+    public float frameTime = 0.01f;
+
+    void Start()
+    {
+        StartCoroutine(PhysicStepper());
+    }
+
+    private IEnumerator PhysicStepper()
+    {
+        while (true)
+        {
+            Physics.Simulate(physicSteps);
+            yield return new WaitForSeconds(frameTime);
+        }
+    }
+}
